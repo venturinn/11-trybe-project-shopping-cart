d8ac6a0e16a8a73c7f39817c1f5c526eb7aa44f6
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-18 13:55:47 -0300

    Adicionado o requisito 10

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,18 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    //const localStorage = jest.fn();
+    getSavedCartItems();
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro.', () => {
+    const localStorage = jest.fn();
+    getSavedCartItems();;
+    expect(localStorage).toHaveBeenCalledWith("cartItems");
+  });
+  
+  //fail('Teste vazio');
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,16 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+ 
+  it('este se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com 02 parâmetros corretos', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith("cartItems", "\"<ol><li>Item</li></ol>\"");
+  });
+
+  // fail('Teste vazio');
 });
